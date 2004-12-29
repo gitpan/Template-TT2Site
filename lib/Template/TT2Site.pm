bc948ab7dc198ac30de7061d2d4a735a041ba1bd
Johan Vromans <jv@cpan.org>
2004-12-29 16:12:34 +0000

    Import of JV/Template-TT2Site-0.90 from CPAN.
    gitpan-cpan-distribution: Template-TT2Site
    gitpan-cpan-version:      0.90
    gitpan-cpan-path:         JV/Template-TT2Site-0.90.tar.gz
    gitpan-cpan-author:       JV
    gitpan-cpan-maturity:     released

--- lib/Template/TT2Site.pm
+++ lib/Template/TT2Site.pm
@@ -3,9 +3,7 @@ package Template::TT2Site;
 use strict;
 use vars qw($VERSION);
 
-$VERSION = sprintf("%d.%02d", q$Revision: 1.5 $ =~ /(\d+)\.(\d+)/);
-
-# NOTE: This is a documentation-only module.
+$VERSION = sprintf("%d.%02d", q$Revision: 1.42 $ =~ /(\d+)\.(\d+)/);
 
 use strict;
 
@@ -21,6 +19,9 @@ Template::TT2Site - Create standard web sites with the Template Toolkit
   ... make your pages ...
   $ tt2site build
   ... point your browser at html/index.html ...
+
+C<tt2site> is just a wrapper program. C<tt2site setup> is equivalent
+to C<perl -MTemplate::TT2Site -e setup>, and so on.
 
 =head1 DESCRIPTION
 
@@ -51,173 +52,59 @@ automatically using minimal, position independent, directions.
 
 =back
 
-=head2 Structure
+This module, B<Template::TT2Site>, provides the necessary methods to
+setup and maintain a site. It is used by the wrapper program,
+B<tt2site>.
 
-A TT2Site site is configured to take templates from two sources: from
-the local directory, and from the C<TT2SITE_LIB>. The templates in the
-C<TT2SITE_LIB> directory provide (almost) all the necessary
-information to create the site (except for the contents, of course),
-the library templates can be overruled locally to customize a
-particular site.
+For more information, see the
+Web site: L<http://www.squirrel.nl/people/jvromans/tt2site/index.html>.
 
-The library templates reside in the C<lib> directory. Its major
-subdirectories are C<config> (configuration data), C<page> (page
-formatting), and C<util> (utility functions).
+=head1 METHODS
 
-For the time being, the contents of the files need to be taken as a
-guide how to write your own. Here is a short description of some
-files:
+The following methods are exported by default.
 
-=over 4
+=over 8
 
-=item site/main
+=item B<setup>
 
-This file controls which other files from the site directory are taken
-into account.
+Initialises a new site directory. This command must be run once before
+you can do anything else.
 
-=item site/config
+=item B<build>
 
-The site data like title and subject. This file must be overridden
-with actual data.
+Run the C<ttree> application to update the site files.
 
-=item site/images
+=item B<rebuild>
 
-The definition of images to be used with the C<util/image> template
-function.
+Run the C<ttree> application to completely rebuild all site files.
 
-=item site/lang
+=item B<clean>
 
-The definition of the languages to be used by this site.
+Cleans the generated HTML files, and editor backup files.
 
-=item page/wrapper
+=item B<realclean>
 
-This fike controls what templates are applied when processing user
-files.
+Cleans the generated HTML files, editor backup files, and all files
+originally installed using the B<setup> command.
 
-=item page/html
-
-This template provides the general HTML structure of the generated
-pages.
-
-=item page/layout
-
-This template defines the structure of the generated pages. It uses
-the familiar table approach with cells containing the logo, header,
-menu, contents, footer, and so on.
+You'll be asked for confirmation before your files are removed.
 
 =back
 
-=head2 Getting Started
+All other methods are for internal use only.
 
-B<Template::TT2Site> needs to be installed before it can be
-used. You can do this by unpacking the kit it in an arbitrary
-location, and issue the following commands:
+=head1 AUTHOR
 
-  perl Build.PL
-  Build
-  Build install
+Johan Vromans <jvromans@squirrel.nl>
 
-(To see what gets installed, and where, use the target C<fakeinstall>
-instead of C<install>).
+=head1 COPYRIGHT
 
-Alternatively, you can use the C<CPAN> or C<CPANPLUS> tools to install
-C<tt2site> for you.
+This programs is Copyright 2004, Squirrel Consultancy.
 
-Note that the last command requires write access to your local Perl
-installtion.
-
-To set up a new C<TT2Site> site, create a new directory. In this
-directory, issue the following command:
-
-  $ tt2site setup
-
-This will populate the directory with the necessary files and
-directories to create the web site. You can now use the C<tt2site>
-program with the following subcommands:
-
-=over 4
-
-=item *
-
-B<build> (default)
-
-Run the C<ttree> application to update all site data.
-
-=item *
-
-B<rebuild>
-
-Run C<ttree> to completely rebuild all site data.
-
-=item *
-
-B<clean>
-
-Cleanup the generated data (and some other things, like editor backup
-files).
-
-=item *
-
-B<realclean>
-
-Cleanup the generated data and the files originally installed with the
-setup. If you modified any of them, you'll lose your changes.
-
-=back
-
-Almost for certain, you'll have to create in the C<lib> directory, the
-templates C<config/site> and C<config/images>. If you want to make a
-multi-language site you need to create an appropriate C<config/lang>.
-But in many cases this will be all you need to get started creating
-your web pages.
-
-=head2 Menu Construction
-
-Most web sites use a menu to navigate. B<Template::TT2Site>
-automatically creates the menu, using B<.map> files placed in the
-directories to navigate. The avantage of B<.map> files is that they
-can easily be maintained with the directory data, and directories can
-be moved to another location taking the map data with them.
-
-The B<.map> files contain lines as follows:
-
-  menu "Title" target
-
-The target will appear in the menu as "Title".
-The target can be an HTML page (if the file I<target>C<.html> exists),
-a directory (if it exists, introducing a new level of navigation),
-otherwise it will be taken to be an arbitrary URL.
-
-In addition, the top level C<.map> must contain an entry
-
-  title "Menu Title"
-
-=head2 Multi-language Sites
-
-To create a multi-language site, specify the supported languages in the
-B<site.lang> variable (file B<config/lang>) using standard ISO codes
-for the languages, e.g.,
-
-  site.languages = [ 'en', 'nl' ]
-
-Consult the supplied B<config/lang> for details.
-
-A multi-language site contains one top-level C<index.html>, and
-sub-trees of information in directories named after the language code.
-When set up this way, each page, e.g., C<en/product/desc.html> will be
-automatically linked to C<nl/product/desc.html>, and vice versa.
-
-=head1 EXAMPLE SITES
-
-Two example sites are included in the distribution. C<sample0> is a
-simple site basically containing this documentation. C<sample1> is the
-same site in multi-language form. To use either site, change to the
-appropriate directory, and issue the commands:
-
-  $ tt2site setup
-  $ tt2site build
-  ... point your browser at html/index.html ...
-  $ tt2site realclean
+This program is free software; you can redistribute it and/or modify
+it under the terms of the Perl Artistic License or the GNU General
+Public License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
 
 =head1 DEPENDENCIES
 
@@ -248,13 +135,13 @@ This product is better than this documentation.
 Johan Vromans (jvromans@squirrel.nl) wrote this software.
 
 Many things were borrowed and adapted from the Template Toolkit
-materials and the Badger book.
+sample materials and the Badger book.
 
 Web site: L<http://www.squirrel.nl/people/jvromans/tt2site/index.html>.
 
 =head1 COPYRIGHT AND DISCLAIMER
 
-This software is Copyright 2004 by Squirrel Consultancy. All
+This software is Copyright 2004-2005 by Squirrel Consultancy. All
 rights reserved.
 
 This program is free software; you can redistribute it and/or modify
@@ -270,6 +157,281 @@ GNU General Public License or the Artistic License for more details.
 
 =cut
 
-1;
+use base qw(Exporter);
+our (@EXPORT) = qw(build setup rebuild clean realclean);
 
-# $Id: TT2Site.pm,v 1.5 2004/12/11 09:22:22 jv Exp $
+my $my_name = __PACKAGE__;
+
+my $realclean = 0;
+my $verbose = 0;		# more verbosity
+
+my $debug = 0;			# debugging
+my $trace = 0;			# trace (show process)
+my $test = 0;			# test mode.
+
+################ Presets ################
+
+my $setupdone = ".setupdone";
+my $ttree = "ttree";
+my $sitelib;
+
+################ The Process ################
+
+use File::Spec;
+use File::Path;
+use File::Find;
+use Carp;
+
+################ Subroutines ################
+
+sub execute {
+    my ($self, @args) = @_;
+    local(@ARGV) = @args;
+    @ARGV = qw(build) unless @ARGV;
+    my $cmdname = lc(shift(@ARGV));
+    my $cmd = __PACKAGE__->can($cmdname) if $cmdname =~ /^[a-z]/;
+    _usage(1) unless $cmd;
+    $cmd->(@ARGV);
+}
+
+sub _preamble($;$) {
+    $my_name .= "::" . shift;
+    _check_lib();
+    _options(@_);
+    _find_ttree();
+    _check_setup() unless @_ && $_[0];
+}
+
+sub setup {
+    _preamble("setup", 1);
+
+    if ( -f $setupdone ) {
+	carp("$my_name: \"setup\" already done\n");
+	return 0;
+    }
+
+    my $dir  = File::Spec->rel2abs(File::Spec->curdir);
+    my $lib  = _cf($sitelib, qw(Template TT2Site));
+    my $skel = _cf($lib, qw(setup data));
+
+    unshift(@ARGV,
+	    '-s', $skel,
+	    '-d', $dir,
+	    '-f', _cf($lib, qw(setup etc ttree.cfg)),
+	    '--define', "dir=$dir",
+	    '--define', "sitelib=". $lib,
+	    '--define', "tmplsrc=src",
+	    '--define', "debug=$debug");
+
+    unshift(@ARGV, "perl", $ttree);
+
+    warn("+ @ARGV\n") if $trace;
+    system $^X @ARGV;
+    croak("$my_name: ttree did not complete\n") if $?;
+    croak("$my_name: ttree did not complete\n")
+      unless -f _cf(qw(etc ttree.cfg));
+
+    chmod(0664, _cf(qw(etc ttree.cfg)));
+    chmod(0664, _cf(qw(src css site.css)));
+    chmod(0664, _cf(qw(src debug.html)));
+    open(my $fh, ">$setupdone");
+
+    return 0;
+}
+
+sub build {
+    _preamble("build");
+
+    my (@args) = qw(-f etc/ttree.cfg);
+
+    unshift(@args, "perl", "-Mlib=$sitelib", $ttree);
+    warn("+ @args\n") if $trace;
+    system $^X @args;
+    croak("$my_name: ttree did not complete\n$@") if $?;
+    return 0;
+}
+
+sub rebuild {
+    _preamble("rebuild");
+
+    my (@args) = qw(-a -f etc/ttree.cfg);
+
+    unshift(@args, "perl", "-Mlib=$sitelib", $ttree);
+    warn("+ @args\n") if $trace;
+    system $^X @args;
+    croak("$my_name: ttree did not complete\n$@") if $?;
+    return 0;
+}
+
+sub publish {
+    _preamble("publish");
+
+    croak("$my_name: \"publish\" not yet implemented\n");
+    return 0;
+}
+
+sub clean {
+    _preamble("clean") unless @_ && $_[0];
+
+    rmtree(["html"], $verbose, 1);
+    find(sub {
+	     if ( /~$/ ) {
+		 warn("+ rm $File::Find::name\n");
+		 unlink($File::Find::name);
+	     }
+	 }, ".");
+    return 0;
+}
+
+sub realclean {
+    _preamble("realclean");
+    print STDERR ("WARNING: ",
+		  "Your customisations to copied files will be lost!\n",
+		  "Hit Enter to continue, Control-C to cancel ");
+    <STDIN>;
+
+    clean(1);
+
+    my @files;
+    my @chfiles;
+    use Cwd;
+    my $cur = getcwd;
+    chdir(_cf($sitelib, qw(Template TT2Site setup data)));
+    find(sub {
+	     return unless -f $_;
+	     push(@{_differ($_, _cf($cur, $File::Find::name))
+		  ? \@chfiles : \@files}, $File::Find::name);
+	 }, ".");
+    chdir($cur);
+
+    if ( @chfiles ) {
+	print STDERR ("WARNING: ",
+		      "The following files were modified:\n",
+		      "\t", join("\n\t", @chfiles), "\n",
+		      "Your changes will be lost!\n",
+		      "Hit Enter to continue, Control-C to cancel ");
+	<STDIN>;
+    }
+
+    foreach my $file ( @files, @chfiles, $setupdone ) {
+	warn("+ rm $file\n");
+	unlink($file);
+    }
+
+    foreach my $dir ( _cf(qw(src images)),
+		      _cf(qw(src css)),
+		      _cf(qw(src)),
+		      _cf(qw(etc)) ) {
+	rmdir($dir) && warn("+ rmdir $dir\n");
+    }
+
+    return 0;
+}
+
+################ Helpers ################
+
+sub _find_ttree {
+    $ttree = "ttree";
+    foreach my $p ( File::Spec->path ) {
+	if ( -s "$p/$ttree.pl" ) {
+	    $ttree = "$p/$ttree.pl";
+	    last;
+	}
+	if ( -s "$p/$ttree" && -x _ ) {
+	    $ttree = "$p/$ttree";
+	    last;
+	}
+    }
+    if ( $ttree eq "ttree" ) {
+	croak("$my_name: Could not find ttree or ttree.pl in PATH\n")
+    }
+    else {
+	open (my $f, "<$ttree") or die("Cannot open $ttree: $!\n");
+	my $line = <$f>;
+	close($f);
+	if ( $line !~ m;^#!.*\bperl\b; ) {
+	    croak("Found ttree in $ttree, but it doesn't seem".
+		  " to be a Perl program.\n",
+		  "TT2Site needs the Perl program to execute.\n",
+		  "Please make it available.\n");
+	}
+    }
+}
+
+sub _check_setup {
+    croak("$my_name: Please execute \"setup\" first\n")
+      unless -f $setupdone;
+}
+
+sub _cf { File::Spec->catfile(@_) }
+
+sub _check_lib {
+
+    my $lib = $ENV{TT2SITE_LIB};
+    if ( $lib ) {
+	unless ( -f _cf($lib, qw(Template TT2Site.pm)) ) {
+	    die("$my_name: Installation problem!\n",
+		"Cannot find Template::TT2Site in $lib\n",
+		"Please verify your installation, or set environment variable ",
+		"TT2SITE_LIB to the directory containing Template/TT2Site.pm\n");
+	}
+	$sitelib = $lib;
+    }
+    else {
+	foreach $lib ( @INC ) {
+	    # warn("Trying: " . _cf($lib, qw(Template TT2Site.pm)) . "\n");
+	    $sitelib = $lib, last if -f _cf($lib, qw(Template TT2Site.pm))
+	}
+    }
+
+    unless ( -f _cf($sitelib, qw(Template TT2Site.pm)) ) {
+	die("$my_name: Installation problem!\n",
+	    "Cannot find Template::TT2Site in $sitelib or \@INC\n",
+	    "Please verify your installation, or set environment variable ",
+	    "TT2SITE_LIB to the directory containing Template/TT2Site.pm\n");
+    }
+}
+
+sub _differ {
+    # Perl version of the 'cmp' program.
+    # Returns 1 if the files differ, 0 if the contents are equal.
+    my ($old, $new) = @_;
+    unless ( open (F1, $old) ) {
+	print STDERR ("$old: $!\n");
+	return 1;
+    }
+    unless ( open (F2, $new) ) {
+	print STDERR ("$new: $!\n");
+	return 1;
+    }
+    my ($buf1, $buf2);
+    my ($len1, $len2);
+    binmode(F1);
+    binmode(F1);
+    while ( 1 ) {
+	$len1 = sysread (F1, $buf1, 10240);
+	$len2 = sysread (F2, $buf2, 10240);
+	return 0 if $len1 == $len2 && $len1 == 0;
+	return 1 if $len1 != $len2 || ( $len1 && $buf1 ne $buf2 );
+    }
+}
+
+################ Command Line Options ################
+
+use Getopt::Long 2.00;
+
+sub _options {
+
+    GetOptions(verbose	   => \$verbose,
+
+	       # development options
+	       test	   => \$test,
+	       trace	   => \$trace,
+	       debug	   => \$debug)
+      or usage(2);
+
+    # Post-processing.
+    $trace |= ($debug || $test);
+}
+
+1;
--- t/basic.t
+++ t/basic.t
@@ -1,17 +1,13 @@
 #!/usr/bin/perl
 
-use Test::More tests => 3;
+use Test::More tests => 2;
 
 BEGIN { use_ok("Template::TT2Site"); }
 BEGIN { use_ok("Template::TT2Site::Plugin::Mapper"); }
 
-if ( $ENV{TT2SITE_LIB} ) {
-    diag("\n".
-	 "You have the environment variable TT2SITE_LIB set.\n".
+if ( 0 && $ENV{TT2SITE_LIB} ) {
+    diag("You have the environment variable TT2SITE_LIB set.\n".
 	 "This is no longer necessary, and may cause malfunctioning.\n".
 	 "Please remove the setting before proceeding.\n");
-    ok(0, "TT2SITE_LIB");
-}
-else {
-    ok(1, "TT2SITE_LIB");
+    fail("TT2SITE_LIB");
 }
